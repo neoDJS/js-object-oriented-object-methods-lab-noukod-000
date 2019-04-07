4b9a0958c9bd59338e6d2ef2fd4682b710c89e08
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-07 15:13:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function BoardMember(name, homeState, training){
   }
   }
   this.approve = function(){
-    return "You can do that!";
-  }
+    return "You will see great things from Scuber.";
   }
   this.sayHi = function(){
     return "Hi, my name is "+this.name+". I am from "+this.homestate+", and I was trained in "+this.training+".";
